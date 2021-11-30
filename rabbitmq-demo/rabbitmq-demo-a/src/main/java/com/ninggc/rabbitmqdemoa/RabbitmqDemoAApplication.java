fdb8cc191645cfd0f69f186f84fa7923a26fe6c8
ninggc <ninggc@foxmail.com>
2021-12-01 00:10:53 +0800

    add rabbitmq-demo

---
+++ rabbitmq-demo/rabbitmq-demo-a/src/main/java/com/ninggc/rabbitmqdemoa/RabbitmqDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.ninggc.rabbitmqdemoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqDemoAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDemoAApplication.class, args);
+    }
+
+}
